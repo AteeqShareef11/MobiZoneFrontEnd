3aefa90109c255acaf33bc4dd9e4dc996b4ae08d
unknown <ateeqshareef11@gmail.com>
2022-10-05 17:56:27 +0500

    sdf

--- src/features/api.js
+++ src/features/api.js
@@ -1,4 +1,7 @@
-export const url = "http://localhost:5000/api";
+const LIVE_SERVER = "https://mobizone.herokuapp.com/api";
+const LOCAL_SERVER="http://localhost:5000/api"
+
+export const url =LIVE_SERVER
 
 export const setHeaders = () => {
   const headers = {
